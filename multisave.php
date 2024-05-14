d91859c43774a5f937727838a2b36297857bcd87
SusiBaril <rosaleskeegan@gmail.com>
2024-05-14 09:12:46 +0800

    yes sir

--- multisave.php
+++ multisave.php
@@ -29,7 +29,7 @@ if (isset($_POST['Multisave'])) {
             // Signup successful, insert address into users_address table
             if ($con->insertAddress($user_id,$street,$barangay,$city,$province)) {
                 // Address insertion successful, redirect to login page
-                header('location:index.php');
+                header('location:login.php');
                 exit();
             } else {
                 // Address insertion failed, display error message
@@ -151,7 +151,7 @@ if (isset($_POST['Multisave'])) {
             <input type="submit" name="Multisave" class="btn btn-outline-primary btn-block mt-4" value="Sign Up">
         </div>
         <div class="col-lg-3 col-md-4"> 
-            <a class="btn btn-outline-danger btn-block mt-4" href="index.php">Go Back</a>
+            <a class="btn btn-outline-danger btn-block mt-4" href="login.php">Go Back</a>
         </div>
     </div>
 </div>
